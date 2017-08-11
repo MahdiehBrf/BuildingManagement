d71263e952a2c7dbbe0d2cc353546fbf1a18b3ef
F_Ghorbanpour <f.gorbanpor@yahoo.com>
2017-08-12 00:19:24 +0430

    Add user login form

--- MyUser/forms.py
+++ MyUser/forms.py
@@ -1,5 +1,4 @@
 import django
-from django.contrib.auth.models import User
 from django import forms
 
 from MyUser.models import Member
@@ -8,7 +7,7 @@ from MyUser.models import Member
 class LoginForm(django.forms.ModelForm):
     class Meta:
         model = Member
-        fields = '__all__'
+        fields = ['username', 'password']
 
 class MessageForm(django.forms.ModelForm):
     class Meta:
