1920b5cb01f86e58452c3ad6a1321d0789d5c613
MahdiehBrf <Mahdieh.brf95@gmail.com>
2017-08-13 11:04:00 +0430

    Paying Reports Process is completed

--- Manager/views.py
+++ Manager/views.py
@@ -9,9 +9,9 @@ from django.utils.datetime_safe import datetime
 from Manager.forms import RequestForm
 from Manager.models import Request
 from MySite.forms import EventForm, NewsForm, DisplayForm
-from MySite.models import Event, News
+from MySite.models import Event, News, Unit
 from MyUser.models import Member
-from Resident.models import Reserve
+from Resident.models import Reserve, PayByBank, PayByAccount
 
 
 @login_required
@@ -82,7 +82,34 @@ def edit_unit(request):
 
 @login_required
 def paying_reports(request):
-    return render(request, 'payingReports.html')
+        bank_reports = None
+        account_reports = None
+        unit = None
+        if request.method == 'POST':
+            form = DisplayForm(request.POST)
+            if form.is_valid():
+                bank_reports = PayByBank.objects.filter(date__gte=form.cleaned_data['startDate'],
+                                               date__lte=form.cleaned_data['finishDate'])
+                account_reports = PayByAccount.objects.filter(date__gte=form.cleaned_data['startDate'],
+                                              date__lte=form.cleaned_data['finishDate'])
+            else:
+                bank_reports = PayByBank.objects.all()
+                account_reports = PayByAccount.objects.all()
+            if request.POST['choose'] == 'جدیدترین':
+                bank_reports = bank_reports.order_by('-date')
+                account_reports = account_reports.order_by('-date')
+            if request.POST['unit']:
+                unit_set = Unit.objects.filter(id=request.POST['unit'])
+                if unit_set:
+                    unit = unit_set[0]
+                    resident = unit.resident
+                    resident_account = resident.account
+                    bank_reports = bank_reports.filter(resident=resident)
+                    account_reports = account_reports.filter(account=resident_account)
+        else:
+            bank_reports = PayByBank.objects.all()
+            account_reports = PayByAccount.objects.all()
+        return render(request, 'payingReports.html', {'account_reports': account_reports, 'bank_reports': bank_reports})
 
 
 @login_required
--- MySite/admin.py
+++ MySite/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from MySite.models import Complex, Block, Board, Event, News, Facility
+from MySite.models import Complex, Block, Board, Event, News, Facility, Unit
 
 admin.site.register(Complex)
 admin.site.register(Block)
@@ -9,3 +9,4 @@ admin.site.register(Board)
 admin.site.register(Event)
 admin.site.register(News)
 admin.site.register(Facility)
+admin.site.register(Unit)
